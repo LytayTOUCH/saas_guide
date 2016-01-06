71ba992588f94f3283e1784b0b5e18fa73761ce2
Lytay TOUCH <lytaytouch@gmail.com>
2016-01-06 10:57:02 +0700

    [Update & CONFIG] Create account model & configure Account model with User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,24 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  validate :email_is_unique, on: :create
+  after_create :create_account
+
   # Set not to send out the email for confirmation (Confirmation is disable by this function)
   def confirmation_required?
     false
+  end
+
+  private
+  # Check unique email in Account Model
+  def email_is_unique
+    unless Account.find_by_email(email).nil?
+      errors.add(:email, " is already taken")
+    end
+  end
+
+  def create_account
+    account = Account.new(:email => email)
+    account.save!
   end
 end
